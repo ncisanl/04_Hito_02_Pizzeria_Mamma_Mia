0920b33508eab15bd05c7ac182f3c52af72f71fd
Nicolás San Martin <nicolas.sm.lor@gmail.com>
2024-09-29 21:22:17 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+import { Home } from "./components/Home";
+
+function App() {
+    return (
+        <>
+            <Navbar />
+            <Home />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
