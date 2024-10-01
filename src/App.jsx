15e3cc8030f6ca9929dfa4542a875c0d4d19da3d
Nicolás San Martin <nicolas.sm.lor@gmail.com>
2024-10-01 12:05:04 -0300

    implementacion de componentes de registro e inicio sesion

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import { Home } from "./components/Home";
+import { Register } from "./components/Register.jsx";
+import { Login } from "./components/Login.jsx";
 
 function App() {
-    return (
-        <>
-            <Navbar />
-            <Home />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <Home />
+      {/*<Register />*/}
+      {/*<Login />*/}
+      <Footer />
+    </>
+  );
 }
 
 export default App;
